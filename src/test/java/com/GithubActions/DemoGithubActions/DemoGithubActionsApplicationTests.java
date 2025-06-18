8647275917a2799483cf7c0a06fa74a3e0c3455d
Joseph Haddison <joseph@edwiki.in>
2025-06-18 14:27:43 -0400

    Added source code

---
+++ src/test/java/com/GithubActions/DemoGithubActions/DemoGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GithubActions.DemoGithubActions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
